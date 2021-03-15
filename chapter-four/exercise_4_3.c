174cd8165f7373e6a62bad49c068c11936f5daa7
jackpan <826647196@qq.com>
2021-03-15 23:21:02 +0800

    Create exercise_4_3.c

---
+++ chapter-four/exercise_4_3.c
@@ -0,0 +1,12 @@
+//
+// Created by JackPan on 2021/3/15.
+//
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3;
+    printf("Enter a three-digit number:");
+    scanf("%1d%1d%1d", &n1, &n2, &n3);
+    printf("The reversal is:%d", n1 + n2 * 10 + n3 * 100);
+}
